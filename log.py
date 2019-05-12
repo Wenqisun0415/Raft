fc815dfe5d8bae855a690d3842d91cc7c1579ab7
Wenqi Sun <wenqisun0415@gmail.com>
2019-05-12 10:55:26 +1000

    configured logging

--- log.py
+++ log.py
@@ -1,15 +1,12 @@
-import pickle
+
 import logging
 
 
 
 class LogManager:
 
-    def __init__(self, log, reset=False):
-        if reset:
-            self.log = []
-        else:
-            self.log = log
+    def __init__(self, log=[]):
+        self.log = log
 
     def append_entry(self, index, entry):
         """
@@ -18,30 +15,13 @@ class LogManager:
         if index == len(self.log) + 1:
             self.log.append(entry)
         elif index <= len(self.log):
-            self.log = self.log[:index-1].append(entry)
+            self.log = self.log[:index-1]
+            self.log.append(entry)
         else:
-            logger.error("Append entry error!")
+            #logger.error("Append entry error!")
+            pass
 
-    @property
-    def log(self):
+    def get_log(self):
         return self.log
-    
-
-def serialize(data):
-    """
-    Store object into the disk
-    data -- the object to be stored
-    """
-    fileObject = open("persist", "wb")
-    pickle.dump(data, fileObject)
-    fileObject.close()
-
-def deserialized():
-    """
-    Retrieve object from the disk
-    """
-    fileObject = open("persist", "rb")
-    data = pickle.load(fileObject)
-    return data
 
 
--- persist.py
+++ persist.py
@@ -1,4 +1,68 @@
+from log import LogManager
+import pickle
+import os.path
+import logging
+import logging.config
+
+logging.config.fileConfig(fname='file.conf', disable_existing_loggers=False)
+logger = logging.getLogger("raft")
 
 class Persist:
 
-    def __init__(self, log_manager,  )
+    def __init__(self, reset=False):
+        if reset or not os.path.isfile("persist"):
+            self.data = {
+                "current_term": 0,
+                "vote_for": None,
+                "log_manager": LogManager()
+            }
+        else:
+            self.data = self.deserialized()
+
+    def increment_term(self):
+        self.data["current_term"] += 1
+        self.serialize()
+        logger.info(f"Current term increased to {self.get_current_term()}")
+
+    def vote(self, candidate):
+        self.data["vote_for"] = candidate
+        self.serialize()
+        logger.info(f"Vote for {self.get_vote_for()}")
+
+    def append_entry(self, index, entry):
+        self.data["log_manager"].append_entry(index, entry)
+        self.serialize()
+        logger.info("New entry appended")
+
+    def get_current_term(self):
+        return self.data["current_term"]
+
+    def get_vote_for(self):
+        return self.data["vote_for"]
+
+    def serialize(self):
+        """
+        Store object into the disk
+        data -- the object to be stored
+        """
+        fileObject = open("persist", "wb")
+        pickle.dump(self.data, fileObject)
+        fileObject.close()
+
+    def deserialized(self):
+        """
+        Retrieve object from the disk
+        """
+        fileObject = open("persist", "rb")
+        data = pickle.load(fileObject)
+        return data
+
+
+def test():
+    persist = Persist()
+    persist.append_entry(2, "update")
+    persist.increment_term()
+    newP = Persist()
+    print(newP.data["log_manager"].get_log())
+
+
