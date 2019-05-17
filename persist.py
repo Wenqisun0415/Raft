89ad7adafba4deab875abebf4209a205a706e733
kataraSSS <50225577+kataraSSS@users.noreply.github.com>
2019-05-17 15:46:59 +1000

    Update persist.py

--- persist.py
+++ persist.py
@@ -1,3 +1,12 @@
+'''
+Project: 
+Distributed KV storage system with Raft algorithm
+
+Author:
+Wenqi Sun 928630	Huiya Chen 894933
+Yishan Shi 883166	Shaobo Wang 935596
+'''
+
 from log import LogManager
 import pickle
 import os.path
