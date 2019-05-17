29353642b2c8ea52540b416e8b7e00637238ff98
kataraSSS <50225577+kataraSSS@users.noreply.github.com>
2019-05-17 15:05:08 +1000

    Update state.py

--- state.py
+++ state.py
@@ -34,7 +34,8 @@ class State:
         called_method(peer, message)
 
     def receive_client_message(self, message, transport):
-        # this 
+        #this method deal with messages received from client.
+        #if the node is not a leader, it returns leader address to client. else calling correspondent methods.
         if self.raft.get_leader() != self.raft.get_address():
             logger.info("Redirecting message to leader")
             new_message = {
@@ -49,13 +50,14 @@ class State:
             called_method(message, transport)
 
 class Follower(State):
-
+#this class is responsible for follower's processing logic.
     def __init__(self, raft):
         super().__init__(raft)
         self.raft.set_vote_for(None)
         self.reset_timer()
 
     def reset_timer(self):
+    # if timeout, follower will start a new countdown. 
         if hasattr(self, "follower_timer"):
             self.follower_timer.cancel()
         
@@ -67,6 +69,8 @@ class Follower(State):
         self.follower_timer.cancel() 
 
     def request_vote(self, peer, message):
+    #when receive vote request from Candidate, reply false if its own term > received term.
+    #if vote_for is None or received "candidate_id", and candidate's log is at least as up-to-date as own's, reply True.
         self.reset_timer()
         can_vote = self.raft.get_vote_for() == None or self.raft.get_vote_for() == message["candidate_id"]
         deny = (self.raft.get_last_log_term() > message["last_log_term"]) or\
@@ -79,10 +83,17 @@ class Follower(State):
         self.raft.send_peer_message(peer, response)
 
     def append_entries(self, peer, message):
+    # receive "append entries" request from leader.
         self.reset_timer()
         self.raft.leader = tuple(message["leader_id"])
-        response = {"type": "append_entries_response", "term": self.raft.get_current_term()}
+               
+        #return current term, for leader to update itself.
+        response = {"type": "append_entries_response", "term": self.raft.get_current_term()}                   
         print("Prev_log_index is {}".format(message["prev_log_index"]))
+                    
+        #if follower's current term > leader's term, return false.
+        #if follower's log does not contain an entry at "prev_log_index" whose term matches "prev_log_term", return false.
+        #else,return true.
         if message["term"] < self.raft.get_current_term():
             response["success"] = False
         elif self.raft.get_log_term(message["prev_log_index"]) != message["prev_log_term"]:
@@ -90,13 +101,14 @@ class Follower(State):
         else:
             response["success"] = True
             logger.info(f"Now appending entries {message['entries']} with index {message['prev_log_index']}")
+            #append new entry
             self.raft.append_entries(message["prev_log_index"], message["entries"])
 
-            #self.raft.commit_index = min(message["prev_log_index"], self.raft.get_last_log_index())
+            #update follower's commit index.
             self.raft.commit_index = min(message["leader_commit"], self.raft.get_last_log_index())
-            #print("Commit index is {}".format(self.raft.commit_index))
+            #apply to state machine according to commit index.
             self.raft.apply_action(self.raft.get_commit_index())
-
+        #return match index to leader.
         response["match_index"] = self.raft.get_last_log_index()
         self.raft.send_peer_message(peer, response)
 
@@ -107,7 +119,7 @@ class Follower(State):
         pass
 
 class Candidate(State):
-
+#this class is responsible for candidate's processing logic.
     def __init__(self, raft):
         super().__init__(raft)
         logger.info("Turning to candidate")
@@ -118,6 +130,7 @@ class Candidate(State):
         self.send_request_vote()
 
     def vote_self(self):
+    #candidate votes itself.
         self.raft.set_vote_for(self.raft.get_address())
         self.received_vote_num += 1
 
@@ -125,6 +138,7 @@ class Candidate(State):
         self.election_timer.cancel()
 
     def reset_election_timer(self):
+    #if timeout, start a new election.
         if hasattr(self, "election_timer"):
             self.election_timer.cancel()
         
@@ -133,6 +147,7 @@ class Candidate(State):
         self.election_timer = loop.call_later(timeout, self.raft.change_state, Candidate)
 
     def send_request_vote(self):
+    # broadcast vote request to all servers.
         logger.info("Sending out request vote")
         message = {
             "type": "request_vote",
@@ -144,6 +159,8 @@ class Candidate(State):
         self.raft.broadcast(message)
 
     def vote_result(self, peer, message):
+    #if "vote_granted" is true, means get one vote.
+    #if more than half of the servers respond with true, turn to leader.
         if message["vote_granted"]:
             self.received_vote_num += 1
             logger.info(f"Current vote number: {self.received_vote_num}")
@@ -152,6 +169,7 @@ class Candidate(State):
                 logger.info("I'm the new leader")
             
     def append_entries(self, peer, message):
+    #if receive 'append entry' request from a leader which has the same term, candidate turns to follower.
         logger.info("Turning to follower due to receiving from leader")
         self.raft.change_state(Follower)
         self.raft.state.append_entries(peer, message)
@@ -163,12 +181,13 @@ class Candidate(State):
         pass
 
 class Leader(State):
-
+#this class is responsible for leader's processing logic.
     def __init__(self, raft):
         super().__init__(raft)
         self.next_index = {}
         self.match_index = {}
         self.waiting_list = {}
+        #initial 'next_index' and 'match_index'.
         for peer in raft.get_cluster():
             self.next_index[peer] = self.raft.get_last_log_index() + 1
             self.match_index[peer] = 0
@@ -180,6 +199,7 @@ class Leader(State):
         self.heartbeat_timer.cancel()
 
     def send_append_entries(self):
+    #send 'append entry' request to all other nodes.
         for peer in self.raft.get_cluster():
             if peer == self.raft.get_address():
                 continue
@@ -197,6 +217,7 @@ class Leader(State):
         self.reset_heartbeat_timer()
         
     def reset_heartbeat_timer(self):
+    #set timeout for heartbeat package, heartbeat packets are sent continuously.
         if hasattr(self, "heartbeat_timer"):
             self.heartbeat_timer.cancel()
         
@@ -205,7 +226,9 @@ class Leader(State):
         self.heartbeat_timer = loop.call_later(timeout, self.send_append_entries)
 
     def append_entries_response(self, peer, message):
+    #receive response of 'append entries' from other nodes.
         if message["success"]:
+        #if append successfully, update leader's 'next_index' (+1) and 'match_index'.
             self.next_index[peer] = message["match_index"] + 1
             self.match_index[peer] = message["match_index"]
             self.match_index[self.raft.get_address()] = self.raft.get_last_log_index()
@@ -213,26 +236,32 @@ class Leader(State):
 
             # The median of the commit_index is the maximum log that appears on majority of servers
             self.raft.commit_index = median(self.match_index.values())
-            #TODO commit
+            # after updating 'commit_index' of leader, apply and response to client.
             self.raft.apply_action(self.raft.get_commit_index())
             self.respond_to_client()
 
         else:
+        #if append unsuccessfully, update leader's 'next_index' (-1).
             self.next_index[peer] -= 1
 
     def client_request(self, message, transport):
+    #receive client's request message.
         entries = [{
             "term": self.raft.get_current_term(),
             "command": message["command"],
             "key": message["key"],
             "value": message["value"] if "value" in message else None
         }]
+        #according to length of log, update waiting list of cllent.
+        #hence, one client can receive its own response of 'append entry'.
         index = self.raft.get_last_log_index()
         self.waiting_list[index] = transport
         self.raft.append_entries(index, entries)
         logger.info("Upload is recorded")
 
     def respond_to_client(self):
+    #after apply several entries to state machine, leader sends responses to correspondent clients.
+    #then delete from waiting list.
         message = {
             "type": "result"
         }
